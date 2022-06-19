f910bc14c7fbb8af9ec13765722e4fbc401c797b
Roman Kulikov <kulikovroman746@gmail.com>
2022-06-19 19:29:52 +0300

    done

---
+++ learn-js/1.2.js
@@ -0,0 +1,11 @@
+// Работа с переменными
+
+let admin, name;
+name = "Джон";
+admin = name;
+console.log(admin);
+
+// Придумайте правильные имена
+
+let ourPlanetName = "";
+let currentUserName = "";
