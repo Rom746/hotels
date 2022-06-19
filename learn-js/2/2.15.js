4add84a9823dedcd86e3d35386b5ce38f0b151dc
Roman Kulikov <kulikovroman746@gmail.com>
2022-06-19 21:35:54 +0300

    done

---
+++ learn-js/2/2.15.js
@@ -0,0 +1,17 @@
+// Перепишите функцию, используя оператор '?' или '||'
+
+function checkAge (age) {
+    return age > 18 ? true : confirm('Родители разрешили?');
+}
+
+// Функция min(a, b)
+
+function min (a, b) {
+    return a < b ? a : b;
+}
+
+// Функция pow(x,n)
+
+function pow (x, n) {
+    return x**n;
+}
