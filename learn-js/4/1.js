4761486117da35d6a4f1216e7d1433640065e0ec
Roman Kulikov <kulikovroman746@gmail.com>
2022-06-19 21:55:25 +0300

    done

---
+++ learn-js/4/1.js
@@ -0,0 +1,57 @@
+// Привет, object
+
+let user = {};
+
+user.name = 'John';
+user.surname = 'Smith';
+user.name = 'Pete';
+delete user.name;
+
+// Проверка на пустоту
+
+function isEmpty(obj) {
+    for (const key in obj) {
+        return false;
+    }
+    return true
+}
+
+let schedule = {};
+console.log(isEmpty(schedule));
+
+schedule["8:30"] = "get up";
+console.log(isEmpty(schedule));
+
+// Сумма свойств объекта
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+let sum = 0;
+
+for (const key in salaries) {
+    sum += salaries[key];
+}
+
+// Умножаем все числовые свойства на 2
+
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+
+function multiplyNumeric (obj) {
+    for (const key in obj) {
+        if (typeof  obj[key] == Number) {
+            obj[key] *= 2;
+        }
+    }
+}
+
+multiplyNumeric(menu);
+
