3718e10d2f2e9eaf72eedbd3d0965921dd72195e
Roman Kulikov <kulikovroman746@gmail.com>
2022-06-20 22:36:25 +0300

    done

---
+++ learn-js/1.8/8.1.js
@@ -0,0 +1,48 @@
+// Алгоритм поиска
+
+let head = {
+    glasses: 1
+};
+
+let table = {
+    __proto__: head,
+    pen: 3
+};
+
+let bed = {
+    __proto__: table,
+    sheet: 1,
+    pillow: 2
+};
+
+let pockets = {
+    __proto__: bed,
+    money: 2000
+};
+
+// Почему наедаются оба хомяка?
+
+let hamster = {
+    stomach: [],
+
+    eat(food) {
+        this.stomach.push(food);
+    }
+};
+
+let speedy = {
+    __proto__: hamster,
+    stomach: []
+};
+
+let lazy = {
+    __proto__: hamster,
+    stomach: []
+};
+
+// Этот хомяк нашёл еду
+speedy.eat("apple");
+console.log(speedy.stomach); // apple
+
+// У этого хомяка тоже есть еда. Почему? Исправьте
+console.log(lazy.stomach); // apple
