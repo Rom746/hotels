b5cceb883e8210dfb519e754307f13b84458d1ad
Roman Kulikov <kulikovroman746@gmail.com>
2022-06-21 20:05:31 +0300

    done

---
+++ learn-js/2.1/1.4.js
@@ -0,0 +1,7 @@
+let table = document.getElementById('age-table');
+let labels = table.getElementsByTagName('label');
+let td = table.querySelector('td');
+let form = document.getElementsByName('search');
+let firstInput = form.querySelector('input');
+let inp = form.querySelectorAll('input')
+let lastInput = inp[inp.length - 1];
