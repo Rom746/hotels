1ad9ddc14234eae021d790a608520b98a1c74079
Roman Kulikov <kulikovroman746@gmail.com>
2022-06-27 21:48:01 +0300

    Добавлен tarifs

--- page-oskolnet/js/main.js
+++ page-oskolnet/js/main.js
@@ -1,57 +1,67 @@
 const createSlider = () => {
-    const slider = document.querySelector('.slider');
+    let count = 0;
+    for (const slider of document.querySelectorAll('.slider')) {
 
-    if (!slider) { return; }
+        if (!slider) { return; }
 
-    const wrapper = document.createElement('div');
-    const ul = document.createElement('ul');
-    const ol = document.createElement('ol');
+        const wrapper = document.createElement('div');
+        const ul = document.createElement('ul');
+        const ol = document.createElement('ol');
 
-    wrapper.className = 'slider__wrapper';
-    ul.className = 'slider__items';
-    ol.className = 'slider__indicators';
 
-    let ind = 1;
+        wrapper.className = 'slider__wrapper';
+        ul.className = 'slider__items';
+        ol.className = 'slider__indicators';
+        ul.dataset.id = count;
+        ol.dataset.id = count++;
 
-    while (slider.children.length) {
-        const item = document.createElement('li');
-        const li = document.createElement('li');
+        let ind = 1;
 
-        item.className = 'slider__item';
-        li.className = 'slider__indicator';
+        while (slider.children.length) {
+            const item = document.createElement('li');
+            const li = document.createElement('li');
 
-        if (ind == 1) {
-            item.classList.add('slider__item--active');
-            li.classList.add('slider__indicator--active')
+            item.className = 'slider__item';
+            li.className = 'slider__indicator';
+
+            if (ind == 1) {
+                item.classList.add('slider__item--active');
+                li.classList.add('slider__indicator--active')
+            }
+
+            item.dataset.ind = li.dataset.toInd = ind++;
+
+            item.append(slider.children[0]);
+            ul.append(item);
+            ol.append(li);
+
+            li.addEventListener('click', moveTo);
         }
 
-        item.dataset.ind = li.dataset.toInd = ind++;
+        wrapper.append(ul);
+        slider.append(wrapper);
+        slider.append(ol);
 
-        item.append(slider.children[0]);
-        ul.append(item);
-        ol.append(li);
-
-        li.addEventListener('click', moveTo);
     }
-
-    wrapper.append(ul);
-    slider.append(wrapper);
-    slider.append(ol);
-
 }
 
 const moveTo = (e) => {
     e.preventDefault();
+    const id = e.target.parentElement.dataset.id;
     const index = parseInt(e.target.dataset.toInd, 10);
-    const carrentSlide = document.querySelector('.slider__item--active');
+    const slider = document.querySelector(`[data-id="${id}"]`);
+    console.log(slider);
+    const carrentSlide = slider.querySelector('.slider__item--active');
     const currentIndex = carrentSlide.dataset.ind;
 
-    if (index == currentIndex) { return ;}
+    if (index == currentIndex) { return; }
 
-    const slide = document.querySelector(`[data-ind="${index}"]`);
-    const slideInd = document.querySelector(`[data-to-ind="${currentIndex}"]`);
+    const slide = slider.querySelector(`[data-ind="${index}"]`);
+    const slideInd = e.target.parentElement.querySelector(`[data-to-ind="${currentIndex}"]`);
 
-    const slider = document.querySelector('.slider__items');
+    console.log(slideInd, currentIndex);
+
+
 
     slide.classList.add('slider__item--active');
     e.target.classList.add('slider__indicator--active');
@@ -69,5 +79,19 @@ const moveTo = (e) => {
 
 }
 
+const setTabs = () => {
+    const tabs = document.querySelectorAll('.tabs__item');
+    tabs.forEach(tab => 
+        tab.addEventListener('click',(event) => {
+            const blockTab = document.querySelector(event.target.dataset.link);
+            document.querySelector('.tabs__block--active').classList.toggle('tabs__block--active');
+            document.querySelector('.tabs__item--active').classList.toggle('tabs__item--active');
+            blockTab.classList.toggle('tabs__block--active');
+            tab.classList.toggle('tabs__item--active');
+        })
+    );
+}
+
 
 createSlider();
+setTabs();
